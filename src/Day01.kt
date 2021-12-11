a8d62494da51cf817035412e0d07987958db6e8b
jebt <roelantvanderhilst@gmail.com>
2021-12-11 16:32:14 +0700

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputValues = input.map { it.toInt() }
+        var increaseCount = 0
+        inputValues.forEachIndexed { index, int ->
+            if (index == 0) {
+                return@forEachIndexed
+            } else {
+                if (int > inputValues[index - 1]) {
+                    increaseCount++
+                }
+            }
+        }
+        return increaseCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputValues = input.map { it.toInt() }
+        var increaseCount = 0
+        inputValues.forEachIndexed { index, int ->
+            if (index < 3) {
+                return@forEachIndexed
+            } else {
+                if (int + inputValues[index-1] + inputValues[index-2] > inputValues[index-1] +
+                            inputValues[index-2] + inputValues[index-3]) {
+                    increaseCount++
+                }
+            }
+        }
+        return increaseCount
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) // 1754
+    println(part2(input)) // 1789
 }
